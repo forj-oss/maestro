70a992bb04cba57875ad2a4941c4180306d1298d
Christophe Larsonneur <larsonneur.christophe@gmail.com>
2014-04-09 14:07:10 +0200

    UserStory#1447 : Preparation work to split maestro and openstack blueprint
    Change-Id: Iaa27e8b9600810fd219b79dfd729018f16ba0b98

---
+++ puppet/manifests/maestro.pp
@@ -0,0 +1,60 @@
+# (c) Copyright 2014 Hewlett-Packard Development Company, L.P.
+# 
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+# 
+#        http://www.apache.org/licenses/LICENSE-2.0
+# 
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# This manifest file is currently working in vagrant orchestrator box.
+# we will port this later to work on a deployed system.
+#
+#
+# Setup host so that we can deal with vagrant configs and ip based configs
+# this section still requires some work
+# TODO: need to figure out if this should be global of not.
+
+if str2bool($::vagrant_guest) == true {
+  $node_server = 'localhost'
+} else {
+  $node_server = inline_template('<% if defined?(@ec2_public_ipv4) %><%= @ec2_public_ipv4 %><% elsif defined?(@ipaddress_eth0)%><%= @ipaddress_eth0 %><% else %><%= @fqdn %><% end %>')
+}
+
+$node_vhost = $node_server
+$sysadmins = []
+
+# implements hiera_data installations
+node default {
+    # global's
+  Exec { path => [
+    '/bin/',
+    '/sbin/',
+    '/usr/bin/',
+    '/usr/sbin/',
+    '/usr/local/bin/'
+    ]
+  }
+  hiera_include('classes')
+}
+
+#
+# This manifest file is currently working in vagrant orchestrator box.
+# we will port this later to work on a deployed system.
+
+node /^maestro.*/ inherits default {
+
+  # moved up sysadmin config due to integration of dashboard and maestro components
+  # main app for this server, the controller ui for cdk
+  notify{ 'maestro ui box execution': } ->
+  ::sysadmin_config::setup { 'setup puppetmaster and dashboard ports':
+      iptables_public_tcp_ports => [4505, 4506, 8139, 8140, 80, 443, 3000, 8080],
+      sysadmins                 => $sysadmins,
+  } ->
+  ::sysadmin_config::swap { '512':}
+}
