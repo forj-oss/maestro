fac1e1137a0eeba78bf4e0d8283ce915d4204ba2
Miguel Quintero <migmaqer@gmail.com>
2014-04-30 09:14:19 -0400

    story #1748: exec commands from nodejs env..
    Change-Id: Ibd0798ff0e9a5fb6fe419a41f3d49008df472b90

--- ui/api/controllers/CommandController.js
+++ ui/api/controllers/CommandController.js
@@ -15,14 +15,31 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
+var maestroexec = require('maestro-exec');
+
 module.exports = {
     
   /**
    * Action blueprints:
    *    `/command/exec`
    */
+   /* if you want to capture command output in data
+    maestro.execCmd('/bin/ls',' -la /home/miqui/tmp', null, function(data, error) {
+        if(error) {
+          console.log('my error:' + error);
+    } else {
+         console.log('my data:' + data);
+    }
+    });
+
+    or
+    like this: to only capture/test data:true/false
+    maestro.execCmd('/bin/ls',' -la /home/miqui/tmp', {status_only: true}, function(data) {
+       console.log('my data:' + data);
+    })
+   */
    exec: function (req, res) {
-    
+
     // Send a JSON response
     return res.json({
       hello: 'world'
