0fd032441e4b2053943c65b90d5d7695457c1316
Edward Raigosa <edward.raigosa@gmail.com>
2014-05-02 16:45:52 -0700

    remove all server using maestro classes, so we can get a good cleanup on dns names as well.
    Change-Id: I9e5eefbe529ad02a530c1f466653726caff7ca60

--- puppet/manifests/maestro-unprovision.pp
+++ puppet/manifests/maestro-unprovision.pp
@@ -1,3 +1,4 @@
+# == maestro unprovision
 # (c) Copyright 2014 Hewlett-Packard Development Company, L.P.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,61 +14,10 @@
 #   limitations under the License.
 #
 #
-# == maestro unprovision
-# Copyright 2012 Hewlett-Packard Development Company, L.P
 #
 # This manifest file is currently working in vagrant orchestrator box.
 
-if str2bool($::vagrant_guest) == true {
-  $node_server = 'localhost'
-} else {
-  $node_server = inline_template('<% if defined?(@ec2_public_ipv4) %><%= @ec2_public_ipv4 %><% elsif defined?(@ipaddress_eth0)%><%= @ipaddress_eth0 %><% else %><%= @fqdn %><% end %>')
-}
-
-$node_vhost = $node_server
 
 node /.*(maestro|precise32).*/ {
-  #
-  # all nodes should meet these requirements.
-  #
-  # global's
-  Exec { path => [
-    '/bin/',
-    '/sbin/',
-    '/usr/bin/',
-    '/usr/sbin/',
-    '/usr/local/bin/'
-    ]
-  }
-
-  # TODO check if calculates or from where gets the values that
-  # I need to retrieve and use for the instance deletion.
-  if empty($instance_id) {
-    $tinst = split($hostname, '[-]')
-    if $tinst[0] == $hostname {
-      # the hostname does not contain a instance part
-      $tdinst = split($fqdn, '[.]')
-      if empty($tdinst[1]) {
-        # the domain does not contain a instance part
-        $instance_id = '42'
-      } else {
-        $instance_id = $tdinst[1]
-      }
-    } else {
-      $instance_id = $tinst[0]
-    }
-  }
-
-
-  notify{ "${hostname} - maestro unprovisionning kit '${instance_id}' ...": } ->
-  pinas { "${hostname} - Destroying":
-    ensure       => absent,
-    instance_id  => $instance_id,
-    nodes        => [
-      'review',
-      'util',
-      'ci'
-      ],
-    do_parallel  => false,
-  }
+  include maestro::orchestrator::unwindallservers
 }
