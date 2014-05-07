0b24dcf1b06ac8612633d1c1689dbc77b98eeecf
Isaias Piña <iparjona@gmail.com>
2014-05-06 21:23:41 -0500

    Fixed lookup paths for manifests when deleting nodes
    Change-Id: Ib4fc129630133455ccd07ae5bb42cbafa8e3adeb

--- bootstrap/bin/pupkillinst.sh
+++ bootstrap/bin/pupkillinst.sh
@@ -4,7 +4,7 @@
 ## xx-util, xx-ci, xx-review (will the instance to clear
 
 function getnst {
-     export PUPPET_MODULES=/etc/puppet/modules:/opt/config/production/modules:/opt/config/production/git/maestro/puppet/modules
+     export PUPPET_MODULES=/etc/puppet/modules:/opt/config/production/modules:/opt/config/production/git/maestro/puppet/modules:/opt/config/production/git/CDK-infra/blueprints/openstack/puppet/modules
      puppet apply --debug --verbose --modulepath=$PUPPET_MODULES /opt/config/production/git/maestro/puppet/manifests/maestro-unprovision.pp
 } 
 
