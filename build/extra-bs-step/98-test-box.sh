8a7bcf4c4044f47e9d02aed0710c38f364e284cb
Edward Raigosa <edward.raigosa@gmail.com>
2014-06-06 14:04:28 -0700

    update maestro files for copyright license updates
    needed to remove sym links because this breaks our
    copyright check
    
    Change-Id: I42b2aa961586880797fc4c56e1ad24a6d9a81f67

--- build/extra-bs-step/98-test-box.sh
+++ build/extra-bs-step/98-test-box.sh
@@ -1,3 +1,18 @@
+
+# (c) Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 # This sequence will wait for a '/tmp/test-box' flag on the new box to go forward on the cloud-init bootstrap sequence.
 # This is used to test bootstrap sequence from a repository updated. See test-box.sh or git-review -x to retrieve values and go forward in running boot sequence.
 
--- build/proto2b/bootstrap/00-functions.sh
+++ build/proto2b/bootstrap/00-functions.sh
@@ -1,1 +1,89 @@
-../../maestro/bootstrap/00-functions.sh
+
+# (c) Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+function GetJson
+{
+ python -c "exec(\"import json\\njson_d=open('$1').read()\\ndata=json.loads(json_d)\\nprint(data['$2'])\")"
+}
+
+# make sure that the passed in gitlink is a valid git repository url
+function GitLinkCheck
+{
+   _LINK=$1
+   if [ "$_LINK" = "" ] || [ "$_LINK" = "default" ] ; then
+     # this is a default git url, return non-zero so the caller knows.
+     return 2
+   fi
+   if [ "$(echo $_LINK | grep '://')" = "" ]
+   then # Check scp link provided
+      if [ "$(echo $_LINK | grep -ie '^.*:[a-z].*$')" = "" ]
+      then
+           echo "ERROR: $_LINK is not a scp valid format."
+           return 1 
+      else
+           return 0
+      fi
+   else
+      # validate we got a valid git URL
+      _LINK_PROTOCOL=$(echo $_LINK | awk -F'://' '{printf $1}')
+      if   [ "$_LINK_PROTOCOL" = "ssh" ]   ||
+           [ "$_LINK_PROTOCOL" = "http" ]  ||
+           [ "$_LINK_PROTOCOL" = "https" ] ||
+           [ "$_LINK_PROTOCOL" = "git" ]   ||
+           [ "$_LINK_PROTOCOL" = "file" ]  ||
+           [ "$_LINK_PROTOCOL" = "SSH" ]   ||
+           [ "$_LINK_PROTOCOL" = "HTTP" ]  ||
+           [ "$_LINK_PROTOCOL" = "HTTPS" ] ||
+           [ "$_LINK_PROTOCOL" = "GIT" ]   ||
+           [ "$_LINK_PROTOCOL" = "FILE" ]  ; then
+           return 0
+       else
+           echo "ERROR: $_LINK does not have a valid protocol for git"
+           return 1 
+       fi
+   fi
+}
+
+exec 6>&1 > >( awk '{ POUT=sprintf("%s - %s",strftime("%F %X %Z",systime()),$0);
+                 print POUT;
+                 print POUT >> "/var/log/cloud-init.log";
+                 fflush("");
+               }') 2>&1
+
+echo "################# BOOT-Ero Start step 1 #################"
+
+set -x
+
+locale-gen en_US
+# TODO: find if we can source meta.js values from facter since we
+#  have all meta.js in facters now.
+if [ -f /config/meta.js ]
+then
+   PREFIX=/config
+fi
+
+if [ ! -f $PREFIX/meta.js ]
+then
+   echo "Boot image invalid. Cannot go on!"
+   exit 1
+fi
+
+
+. /etc/environment
+_PROXY="$(GetJson /meta-boot.js webproxy)"
+
+apt-get purge -yq python-pip
+
+export HOME=/root
