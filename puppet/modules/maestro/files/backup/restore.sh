f09e313f69913c00dc1ab6eeaed02eabf50ee501
Christophe Larsonneur <larsonneur.christophe@gmail.com>
2014-06-18 16:41:09 +0200

    Fixing license display issue (copy/paste issue)
    Change-Id: I494128cc23101b6593eef7a8e3920146b10e47ef

--- puppet/modules/maestro/files/backup/restore.sh
+++ puppet/modules/maestro/files/backup/restore.sh
@@ -2,19 +2,17 @@
 # (c) Copyright 2014 Hewlett-Packard Development Company, L.P.
 
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# restore.sh script, placed in box to perform backup restore of App files.
-# used to run in dependency of restoreraid 
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
 #
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 
 #--- Variables sets by call of restoreraid master to perfom the restore
 appname="$1"                 #--- Application name; sets the name for the restore folder
