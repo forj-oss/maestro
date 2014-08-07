a61ffbf88cfd956283bfcad0f94fd4a9f4e7da78
Edward Raigosa <edward.raigosa@gmail.com>
2014-08-07 16:29:29 -0700

    user story #2174: correct problem with not getting errors
    cacerts_getkey should throw some warning messages when it
    runs into problem or exceptions.
    Add some testing so we can verify our changes is happy.
    
    Change-Id: I43ddd14f214576a1e090eaa36c8c5436dd44e5d3

--- puppet/modules/cacerts/spec/functions/cacerts_getkey_spec.rb
+++ puppet/modules/cacerts/spec/functions/cacerts_getkey_spec.rb
@@ -1,8 +1,3 @@
-# ***************************************************************
-
-# ** make_keys module for puppet
-# ***************************************************************
-
 # (c) Copyright 2014 Hewlett-Packard Development Company, L.P.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,17 +12,19 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
+require 'spec_helper'
 
- module Puppet::Parser::Functions
-  newfunction(:cacerts_getkey, :type => :rvalue) do |args|
-    data = ''
-    begin
-      filename = args[0]
-      data = File.read(filename) if File.exists?(filename)
-    rescue Exception => e
-       Puppet.warning "failed to read key #{file}"
-       Puppet.warning "#{e}"
+# Test the specification for compute_public_ip_lookup
+
+describe 'cacerts_getkey', :default => true do
+  context 'with default values' do
+    it "will read file" do
+      should run.with_params('./spec/fixtures/test_file.txt').and_return("found")
     end
-    data
+  end
+  context 'with no file to find' do
+    it "will read file" do
+      should run.with_params('./spec/fixtures/noexist').and_return("")
+    end
   end
 end
