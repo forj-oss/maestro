7730a86cfa9033582712ad886baad81720739004
Oscar Romero <homeless@hp.com>
2014-08-12 13:09:47 -0500

    Switch repo location from jenkins.cdkdev.org:82/ubuntu/config to review.forj.io/oo-infra/config
    - switch the clone string
      from: http://jenkins.cdkdev.org:82/ubuntu/config.git
      to: https://review.forj.io/oo-infra/config
    
    this way we can start having a better versioning of this repo using the code review from gerrit.
    
    Change-Id: I3fa581958ca9fe8c0b56c6e15def087f166ad2b2

--- build/maestro/bootstrap/10-openstack-config.sh
+++ build/maestro/bootstrap/10-openstack-config.sh
@@ -15,14 +15,14 @@
 #
 # Configure Openstack config from a freezed version maintained by FORJ team.
 set -x
-echo "host jenkins.cdkdev.org
-     StrictHostKeyChecking=no" >> ~/.ssh/config
+#echo "host jenkins.cdkdev.org
+#     StrictHostKeyChecking=no" >> ~/.ssh/config
 
 export HOME=/root
 # Uses local stable resource instead of internet one. See ubuntu@jenkins.cdkdev.org
 
 cd /opt/config/production/git
-CloneRepo config http://jenkins.cdkdev.org:82/ubuntu/config.git $GITBRANCH
+CloneRepo config https://review.forj.io/oo-infra/config $GITBRANCH
 cd config
 git config core.autocrlf false
 set +x
